908587f27748ed2bd6cba8bc320f2d668d134c23
Hussein <7ssooony14@gmail.com>
2023-04-17 00:35:00 +0300

    Build a navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Input, IconButton } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image></Image>
+      <Image src={logo} boxSize="60px" />
+      <Input placeholder="Basic usage" />
+      <IconButton aria-label="Search database" />
     </HStack>
   );
 };
