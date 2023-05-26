4384dd828649a90c5b2c31ac37ddb38b535b6928
hussein <7ssooony14@gmail.com>
2023-05-27 02:17:16 +0300

    add: initial files

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
     params:{
-        key:'1fefb4a64709486dac6718b197c37a1f'
+        key:'091b6b2761b14b91800c48b1d32ef4ce'
     }
 })
